b3a1e3d9f76ed696a6290e320fcfaaed6e8e1b65
GG.Pan <gg.pan@foxmail.com>
2019-11-19 22:41:24 +0800

    change test.txt

--- qwq.py
+++ qwq.py
@@ -3,14 +3,14 @@
 
 from time_converter import TimeNormalizer  # 引入包
 from time_converter.log import Time_NLP_LOGGER
-Time_NLP_LOGGER.setLevel(10)
+# Time_NLP_LOGGER.setLevel(10)
 tn = TimeNormalizer(isPreferFuture=False)
 
 
 def gao():
     tn = TimeNormalizer()
 
-    with open('test.txt', 'r+', encoding='utf-8') as f:
+    with open('test_old.txt', 'r+', encoding='utf-8') as f:
         for line in f:
             res = tn.parse(target=line.strip()) # target为待分析语句，timeBase为基准时间默认是当前时间
             print('Sentence:',line)
@@ -25,5 +25,5 @@ def test():
         print(res)
 
 if __name__ == "__main__":
-    # gao()
-    test()
+    gao()
+    # test()
